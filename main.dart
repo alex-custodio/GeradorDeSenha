2064ecdc7989828772f090137bbcce92a9063a94
alexcstd <alex.scustod@gmail.com>
2023-01-12 15:29:35 -0300

    novas mudanças

--- main.dart
+++ main.dart
@@ -5,18 +5,48 @@ import 'dart:math';
 void main() {
   print("Quantos caracteres você quer na sua senha?");
   int qtdCaracteres = int.parse(stdin.readLineSync()!);
-  gerarSenha(tamanho: qtdCaracteres);
+  print(
+      "Escolha uma opção: \n1 - (Letras maiúsculas, minúsculas, números e caracteres especiais) -> Mais seguro \n2 - (Apenas números)");
+  int opcao = int.parse(stdin.readLineSync()!);
+
+  gerarSenha(tamanho: qtdCaracteres, opcao: opcao);
 }
 
-void gerarSenha({int? tamanho}) {
+void gerarSenha({int? tamanho, int? opcao = 1}) {
   String senha = "";
-  String caracteres =
-      "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz123456789012345678901234567890[][][];;;...,,,---???@@@###";
+  String letras_maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  String letras_minusculas = letras_maiusculas.toLowerCase();
+  String numeros = "1234567890";
+  String outros = "[]#@.,-;/=+";
   for (int i = 0; i < tamanho!; i++) {
-    //Processo
-    String caractere_escolhido =
-        caracteres[Random().nextInt(caracteres.length)];
-    senha += caractere_escolhido;
+    if (opcao == 1) {
+      int modo = Random().nextInt(5);
+      switch (modo) {
+        case 1:
+          String caractere_escolhido =
+              letras_maiusculas[Random().nextInt(letras_maiusculas.length)];
+          senha += caractere_escolhido;
+          break;
+        case 2:
+          String caractere_escolhido =
+              letras_minusculas[Random().nextInt(letras_minusculas.length)];
+          senha += caractere_escolhido;
+          break;
+        case 3:
+          String caractere_escolhido =
+              numeros[Random().nextInt(numeros.length)];
+          senha += caractere_escolhido;
+          break;
+        case 4:
+          String caractere_escolhido = outros[Random().nextInt(outros.length)];
+          senha += caractere_escolhido;
+          break;
+      }
+    } else if (opcao == 2) //Apenas números
+    {
+      String caractere_escolhido = numeros[Random().nextInt(numeros.length)];
+      senha += caractere_escolhido;
+    }
   }
   print(senha);
 }
